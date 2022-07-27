70e6ba29f66c8f2aebe51f2364da2ba6151458e4
Lucas Paulino <lucashsp12@hotmail.com>
2022-07-27 19:23:53 -0300

    mudanças no formulário

--- formularioJS/script.js
+++ formularioJS/script.js
@@ -1,11 +1,20 @@
 const nome = document.getElementById('nome');
 const email = document.getElementById('email');
-const enviar = document.getElementById('enviar');
+const enviarButton = document.getElementById('enviar');
+const lista = document.querySelector('.lista');
 
-enviar.addEventListener('click', function(){
-  if(nome.value === '' || email.value === ''){
-    return alert('Preencha os campos antes de prosseguir!');
-  } else{
-    
+enviarButton.addEventListener('click', function(){
+  if(nome.value === '' || nome.value === ''){
+    return alert('Por favor, preencha os campos solicitados!');
+  } else {
+    const li = document.createElement('li');
+    li.classList = 'lista';
+    li.innerHTML = `Nome: ${nome.value}<br/>Email: ${email.value}`;
+    console.log(li);
+
+    lista.appendChild(li);
   }
+
+  nome.value = '';
+  email.value = '';
 });
